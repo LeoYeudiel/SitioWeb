f211fc8d0a133fa9b38db5af2256da0c76dbd1a6
LeoYeudiel <lyeuprsw@gmail.com>
2023-07-12 17:10:39 -0600

    finished nav

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 const menu = () => {
-  if (document.getElementsByClassName('open')) {
-    alert('hola')
+  let button = document.getElementById('hamburguer')
+  let menu = document.getElementById('menu')
+
+  if (button.classList.contains('open') && menu.classList.contains('open')) {
+    button.classList.add('close');
+    button.classList.remove('open');
+    menu.classList.add('close');
+    menu.classList.remove('open');
+  } else if (button.classList.contains('close') && button.classList.contains('close')) {
+    button.classList.add('open');
+    button.classList.remove('close');
+    menu.classList.add('open');
+    menu.classList.remove('close');
   }
 }
